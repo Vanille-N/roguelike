f1ecb11f55d51c6fcba80c1b861b32e45a70a637
Vanille-N <neven.villani@gmail.com>
2021-02-01 08:19:40 +0100

    first draft for organism tree

--- src/main/scala/entities.scala
+++ src/main/scala/entities.scala
@@ -6,98 +6,98 @@ import java.awt.{ Color, Font }
 import java.lang.System
 import event._
 
-abstract class Actor {
-    var position: Cell = null
-    def symbol: Symbol
+object Alignment extends Enumeration {
+    type Alignment = Value
+    val HOSTILE, NEUTRAL, FRIENDLY = Value
+}
+import Alignment._
 
-    var life = 1
-    def isHero: Boolean = { return false }
-
+abstract class Organism {
+    var position: Pos = null
+    def alignment: Alignment
     def name: String
-    override def toString: String = { if (isHero) "you" else "the " + name }
-
-    def placeOnMap (c: Cell) { position = c; c.setActor(this) }
-
-    // react to music, do nothing by default
-    def hear: Unit = { }
-
-    // take a hit from another actor, possibly die
-    def hit {
-        life -= 1; if (life > 0) return
-        position.removeActor
+    def placeOnMap (p: Pos) {
+        position = p
+        p.addOrganism(alignment, this)
     }
-
-    ///////////////////////////////////////////////////////////////
-    // Movement-related code: Castle object will call move(), which
-    // calls react() as a function of the actor's speed; react()
-    // is supposed to contain the actor-specific code.
-    // move and react return true if something "important" happens.
-
-    var speed = 100        // speed relative to player
-    var residualSpeed = 0
-
-    def move: Boolean = {
-        var retval = false
-        residualSpeed += speed
-        while (residualSpeed >= 100)
-            { residualSpeed -= 100; retval = react || retval }
-        return retval
-    }
-
-    // by default, do nothing
-    def react: Boolean = { return false }
+    var stats: Stats
+    var strength: Int
 }
 
-class Hero extends Actor {
-    val symbol = new Symbol('H',Color.white)
-    override val name = "you"
-    var hasWeapon = false
-    override def isHero: Boolean = { return true }
-
-    life = 5
+class Virus extends Organism {
+    def alignment = FRIENDLY
+    def name = "virus"
+    var stats = new Stats()
+    var strength = stats.toStrength
 }
 
-abstract class AgressiveActor extends Actor {
-    var enemy: Actor = null
-
-    def setEnemy (a: Actor) = { enemy = a }
-
-    // go after the enemy and attack if the enemy is next to the actor
-    def pursuit: Boolean = {
-        if (enemy == null) return false
-        if (enemy.life == 0) { enemy = null; return false }
-
-        val next = position.room.cells.neighbours(position).
-                    minBy(_.l2dist(enemy.position))
-
-        if (next.actor == null) {
-            next.floor.enter(this,next)
-            return false
-        }
-
-        if (next.actor == enemy) {
-            enemy.hit
-            return true
-        }
-
-        return false
-    }
+abstract class Cell extends Organism {
+    def alignment = HOSTILE
+    def name = "cell"
+    var stats = new Stats()
+    var strength = stats.toStrength
 }
 
-class Fairy extends Actor {
-    val symbol = new Symbol('F', Color.green)
-    override val name = "fairy"
-
-    override def hear: Unit = {
-        position.removeActor
-    }
+class WhiteCell extends Cell {
+    override def name = "white cell"
 }
 
-class Gnome extends AgressiveActor {
-    val symbol = new Symbol('G',Color.magenta)
-    override val name = "gnome"
-    life = 3
-    speed = 70
-
-    override def react: Boolean = { return pursuit }
+class RedCell extends Cell {
+    override def name = "red cell"
+    override def alignment = NEUTRAL
 }
+
+//
+// class Hero extends Actor {
+//     val symbol = new Symbol('H',Color.white)
+//     override val name = "you"
+//     var hasWeapon = false
+//     override def isHero: Boolean = { return true }
+//
+//     life = 5
+// }
+//
+// abstract class AgressiveActor extends Actor {
+//     var enemy: Actor = null
+//
+//     def setEnemy (a: Actor) = { enemy = a }
+//
+//     // go after the enemy and attack if the enemy is next to the actor
+//     def pursuit: Boolean = {
+//         if (enemy == null) return false
+//         if (enemy.life == 0) { enemy = null; return false }
+//
+//         val next = position.room.cells.neighbours(position).
+//                     minBy(_.l2dist(enemy.position))
+//
+//         if (next.actor == null) {
+//             next.floor.enter(this,next)
+//             return false
+//         }
+//
+//         if (next.actor == enemy) {
+//             enemy.hit
+//             return true
+//         }
+//
+//         return false
+//     }
+// }
+//
+// class Fairy extends Actor {
+//     val symbol = new Symbol('F', Color.green)
+//     override val name = "fairy"
+//
+//     override def hear: Unit = {
+//         position.removeActor
+//     }
+// }
+//
+// class Gnome extends AgressiveActor {
+//     val symbol = new Symbol('G',Color.magenta)
+//     override val name = "gnome"
+//     life = 3
+//     speed = 70
+//
+//     override def react: Boolean = { return pursuit }
+// }
