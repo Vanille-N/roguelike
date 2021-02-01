5da4e508b5c41728a74a257dc14df4025db78203
Vanille-N <neven.villani@gmail.com>
2021-02-01 10:15:47 +0100

    player management

---
+++ src/main/scala/movement.scala
@@ -0,0 +1,29 @@
+import Math._
+import scala.collection.mutable.Buffer
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.Set
+import scala.swing._
+import javax.swing.BorderFactory._
+import java.awt.{ Color, Font }
+import java.lang.System
+import event._
+
+import Direction._
+
+class Player (var position: Pos) {
+
+    def placeOnMap (p: Pos) {
+        position = p
+        p.isFocused = true
+    }
+    placeOnMap(position)
+
+    def move (dir: Direction): Boolean = {
+        val newPosition = position.tryAdd(dir)
+        if (newPosition != null) {
+            position.isFocused = false
+            placeOnMap(newPosition)
+            true
+        } else false
+    }
+}
