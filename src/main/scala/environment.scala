564eca70bcd72e095824a8dd87c9f76c5fb9b1cb
Vanille-N <neven.villani@gmail.com>
2021-02-01 08:20:21 +0100

    Cell is taken, rename to Pos

--- src/main/scala/environment.scala
+++ src/main/scala/environment.scala
@@ -1,17 +1,17 @@
 import Math._
 import scala.collection.mutable.Buffer
 import scala.collection.mutable.HashMap
+import scala.collection.mutable.Set
 import scala.swing._
 import javax.swing.BorderFactory._
 import java.awt.{ Color, Font }
 import java.lang.System
 import event._
 
+import Alignment._
+
 abstract class Floor {
     def color: Color
-
-    // true if entering ought to stop auto-move; block by default
-    def enter (actor: Actor, cell: Cell): Boolean = { return true }
 }
 
 // Wall and Ground are a single object to avoid instantiating one Floor
@@ -21,14 +21,6 @@ object Wall extends Floor { val color = Color.red }
 
 object Ground extends Floor {
     val color = Color.black
-
-    // no problem entering here
-    override def enter (actor: Actor, cell: Cell): Boolean = {
-        // move the actor
-        actor.position.removeActor
-        actor.placeOnMap(cell)
-        return false
-    }
 }
 
 /****************************************************************************/
@@ -36,25 +28,29 @@ object Ground extends Floor {
 // This class represents one dungeon cell with a floor, optionally an actor
 // and an item. Handles the visual appearance and user-interface aspects.
 
-class Cell (val room: Room, val y: Int, val x: Int) extends Button {
+class Pos (val room: Room, val y: Int, val x: Int) extends Button {
     var floor: Floor = null
-    var actor: Actor = null
+    var friendlyOrganisms: Set[Organism] = Set()
+    var hostileOrganisms: Set[Organism] = Set()
+    var neutralOrganisms: Set[Organism] = Set()
+    var friendlyStrength: Int = 0
+    var hostileStrength: Int = 0
+    var neutralStrength: Int = 0
 
     def setFloor (f: Floor) = { floor = f; update }
-    def removeActor () = { actor = null; update }
-    def setActor (a: Actor) = { actor = a; update }
-
-    // number of moves towards c
-    def linfdist (c: Cell) = max(abs(c.x - x), abs(c.y - y))
-
-    // Euclidian distance towards c
-    def l2dist (c: Cell) = sqrt((c.x - x) * (c.x - x) + (c.y - y) * (c.y - y))
-
-    // Return whether the cell has a normal floor and no actor or item.
-    def isFree: Boolean = {
-        if (actor != null) return false
-        floor match { case Ground => return true }
-        return false
+    def removeOrgasism (alignment: Alignment, x: Organism) = {
+        alignment match {
+            case FRIENDLY => { friendlyOrganisms.remove(x); friendlyStrength -= x.strength }
+            case HOSTILE => { hostileOrganisms.remove(x); hostileStrength -= x.strength }
+            case NEUTRAL => { neutralOrganisms.remove(x); neutralStrength -= x.strength }
+        }
+    }
+    def addOrganism (alignment: Alignment, x: Organism) = {
+        alignment match {
+            case FRIENDLY => { friendlyOrganisms.add(x); friendlyStrength += x.strength }
+            case HOSTILE => { hostileOrganisms.add(x); hostileStrength += x.strength }
+            case NEUTRAL => { neutralOrganisms.add(x); neutralStrength += x.strength }
+        }
     }
 
     // visual appearance
@@ -70,7 +66,6 @@ class Cell (val room: Room, val y: Int, val x: Int) extends Button {
         }
 
         text = ""; background = floor.color
-        if (actor != null) copySymbol(actor.symbol)
     }
 
     // user interface
@@ -88,11 +83,10 @@ class Cell (val room: Room, val y: Int, val x: Int) extends Button {
 
 // Handle a grid of dungeon cells, facilitating some aggregate functions
 class Grid (room: Room, rows: Int, cols: Int) {
-    val elem = IndexedSeq.tabulate(rows, cols) {(i, j) => new Cell(room, i, j)}
-    def map[U] (f: Cell => U) = elem.map(_.map(f(_)))
-    def filter (f: Cell => Boolean) = elem.flatten.filter(f(_))
+    val elem = IndexedSeq.tabulate(rows, cols) {(i, j) => new Pos(room, i, j)}
+    def map[U] (f: Pos => U) = elem.map(_.map(f(_)))
+    def filter (f: Pos => Boolean) = elem.flatten.filter(f(_))
     def apply (i: Int, j: Int) = elem(i)(j)
-    def neighbours (c: Cell) = elem.flatten.filter(c.linfdist(_) <= 1)
 }
 
 /****************************************************************************/
@@ -102,25 +96,25 @@ class Grid (room: Room, rows: Int, cols: Int) {
 
 class Room (val castle: Castle, val cols: Int, val rows: Int)
 extends Reactor with Publisher {
-    var cells = new Grid(this, rows, cols)
+    var locs = new Grid(this, rows, cols)
 
-    cells.map(_.setFloor(Ground))
-    cells.map(listenTo(_))
+    locs.map(_.setFloor(Ground))
+    locs.map(listenTo(_))
 
     reactions += {
-        case leftClicked(c: Cell) => { publish(moveTo(c)) }
+        case leftClicked(c: Pos) => { publish(moveTo(c)) }
     }
 
-    def makeWall (c: Cell, d: Cell) {
+    def makeWall (c: Pos, d: Pos) {
         for (x <- c.x to d.x; y <- c.y to d.y)
-            { cells(y,x).setFloor(Wall) }
+            { locs(y,x).setFloor(Wall) }
     }
 }
 
 class PlainRoom (castle: Castle, cols: Int, rows: Int)
 extends Room (castle,cols,rows) {
-    makeWall(cells(0, 0), cells(0, cols-1))
-    makeWall(cells(0, 0), cells(rows-1, 0))
-    makeWall(cells(rows-1, 0), cells(rows-1, cols-1))
-    makeWall(cells(0, cols-1), cells(rows-1, cols-1))
+    makeWall(locs(0, 0), locs(0, cols-1))
+    makeWall(locs(0, 0), locs(rows-1, 0))
+    makeWall(locs(rows-1, 0), locs(rows-1, cols-1))
+    makeWall(locs(0, cols-1), locs(rows-1, cols-1))
 }
