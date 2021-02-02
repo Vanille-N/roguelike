47b2ccc177a95f47cfab636291e541de70febbde
DABY-SEESARAM Arnaud <daby-seesaram@dptinfo.ens-cachan.fr>
2021-02-02 10:47:16 +0100

    set Health

--- src/main/scala/commands.scala
+++ src/main/scala/commands.scala
@@ -22,21 +22,67 @@ class Command (val castle:Castle, val room: Room, val player: Player) {
         room.locs.map(_.update)
     }
 
-    def commandRequest (s: String) = {
-        if (s != "") castle.logs.text += "$ " + s
-        s.split(" ")(0) match  {
-            case "Up" =>    { tryMove(UP) }
-            case "Down" =>  { tryMove(DOWN) }
-            case "Right" => { tryMove(RIGHT) }
-            case "Left" =>  { tryMove(LEFT) }
-            case "quit" =>  { sys.exit(0) }
-            case "q" =>     { castle.globalPanel.requestFocusInWindow() }
-            case "clear" => { castle.logs.text = "" }
-            case "set" =>   {}
-            case "" =>      {}
-            case _ =>       { castle.logs.text += "\t> command not found ;/\n" }
+    def trySet (args: Array[String]) : Unit = {
+        args(0) match {
+            case "health" => {
+                val pos_x = args(1).toInt
+                val pos_y = args(2).toInt
+                val target_health = args(3).toInt
+                var target_organism: Organism = null
+
+                val tmp_str = room.locs(pos_x, pos_y).listContents
+
+                castle.logs.text += "\n" + tmp_str
+                if(tmp_str != "At position (" + pos_y + "," + pos_x + ")\n  empty\n") {
+                    val target_int: Int = this.commandRequest("\nWhat cell do you choose ?\n" + prompt).toInt
+
+                    var i = 0
+                    if (room.locs(pos_x, pos_y).organisms(1).size > 0) {
+                        room.locs(pos_x, pos_y).organisms(1).foreach(o => {
+                            if(i == target_int) { target_organism = o }
+                            i += 1
+                            })
+                    }
+                    if (room.locs(pos_x, pos_y).organisms(0).size > 0) {
+                        room.locs(pos_x, pos_y).organisms(0).foreach(o => {
+                            if(i == target_int) { target_organism = o }
+                            i += 1
+                            })
+                    }
+
+                    if(target_organism == null) {
+                        castle.logs.text += "\nError : unable to locate target organism :/"
+                    } else {
+                        target_organism.stats.health = target_health
+                    }
+                }
+            }
+            case _ => { castle.logs.text += "\n\tUnknown setting :/" }
+        }
+    }
+
+    def commandRequest (s: String): String = {
+        var ret = ""
+        if (status == 0 ) {
+            if (s != "") castle.logs.text += "$ " + s
+            s.split(" ")(0) match  {
+                case "Up" =>    { tryMove(UP) }
+                case "Down" =>  { tryMove(DOWN) }
+                case "Right" => { tryMove(RIGHT) }
+                case "Left" =>  { tryMove(LEFT) }
+                case "quit" =>  { sys.exit(0) }
+                case "q" =>     { castle.globalPanel.requestFocusInWindow() }
+                case "clear" => { castle.logs.text = "" }
+                case "set" =>   {}
+                case "" =>      {}
+                case _ =>       { castle.logs.text += "\t> command not found ;/\n" }
+            }
+        } else {
+            castle.logs.text += "\n" + "?" + prompt + s
+            ret = castle.cmdline.text
         }
         castle.cmdline.text = ""
+        ret
     }
 }
 // vim: set expandtab tabstop=4 shiftwidth=4 :
