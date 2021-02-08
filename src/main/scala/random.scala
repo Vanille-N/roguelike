d2ba06d9664921bdc6ec75d9c28e5b96e2dca7ef
Vanille-N <neven.villani@gmail.com>
2021-02-08 15:51:50 +0100

    choice with priority and decisiveness

--- src/main/scala/random.scala
+++ src/main/scala/random.scala
@@ -7,5 +7,13 @@ object Rng extends Random {
 
     def choice (p: Double): Boolean = {
         p >= (this.nextInt / 100)
+    def priorityChoice[T] (options: Iterable[T], p: Double): Option[T] = {
+        var buf: Buffer[Tuple2[Double, T]] = Buffer()
+        var q = 1.0
+        options.iterator.foreach(x => {
+            buf.append(Tuple2(q, x))
+            q *= 1.0 - p
+        })
+        weightedChoice(buf)
     }
 }
