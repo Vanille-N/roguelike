1e194cc4b290f834d7e93a04e06427e473ca75ea
Vanille-N <neven.villani@gmail.com>
2021-02-25 09:48:25 +0100

    adjust init of pathfinder : no leaking temporary variables

--- src/main/scala/room.scala
+++ src/main/scala/room.scala
@@ -15,35 +15,28 @@ import scala.io.Source
 // The whole room
 class Room (val body: BodyPart, fbase: String)
 extends Reactor with Publisher {
-    // initialization from src file
-    var rows = 0
-    var cols = 0
-    var locs = new Grid(this, 0, 0)
-
     val wallSpawner = new DefaultWallCellSpawner()
-    def makeWall (p: Pos, q: Pos) = {
-        for (i <- p.i to q.i; j <- p.j to q.j) {
-            wallSpawner.spawn(locs(i, j))
-        }
-    }
     val redCellSpawner = new DefaultRedCellSpawner()
     val whiteCellSpawner = new DefaultWhiteCellSpawner()
     val virusSpawner = new DefaultVirusSpawner()
 
-    def init {
+    // initialization from src file
+    val (rows, cols, locs, pathFinder) = {
         val src = Source.fromFile("assets/" + fbase + ".room")
         val lines = src.getLines
         val dimensions = lines.next.split(" ")
-        rows = dimensions(0).toInt
-        cols = dimensions(1).toInt
-        locs = new Grid(this, rows, cols)
-        for (i <- 0 to rows-1) {
+        val rows = dimensions(0).toInt
+        val cols = dimensions(1).toInt
+        val locs = new Grid(this, rows, cols)
+        val availability = Array.ofDim[Boolean](rows, cols)
+        for (i <- 0 to rows - 1; j <- 0 to cols - 1) availability(i)(j) = true
+        for (i <- 0 to rows - 1) {
             val line = lines.next
             for (j <- 0 to cols-1) {
-                line(j) match {
+                line(2*j) match {
                     case ' ' => ()
                     case '*' => locs(i, j).setFriendlySpawner(new PhysicalSpawner(virusSpawner, 0.015, 10))
-                    case '#' => wallSpawner.spawn(locs(i, j))
+                    case '#' => { wallSpawner.spawn(locs(i, j)); availability(i)(j) = false }
                     case 'R' => locs(i, j).setHostileSpawner(new PhysicalSpawner(redCellSpawner, 0.03, 7))
                     case 'W' => locs(i, j).setHostileSpawner(new PhysicalSpawner(whiteCellSpawner, 0.02, 5))
                 }
@@ -51,13 +44,19 @@ extends Reactor with Publisher {
         }
         src.close
         val pathFinder = new PathFinder(availability, rows, cols)
+        (rows, cols, locs, pathFinder)
     }
-    init
 
     locs.map(listenTo(_))
 
     reactions += {
         case leftClicked(c: Pos) => { publish(displayContents(c)) }
+    }
+
+    def makeWall (p: Pos, q: Pos) = {
+        for (i <- p.i to q.i; j <- p.j to q.j) {
+            wallSpawner.spawn(locs(i, j))
+        }
     }
 
     def addOrganism (o: Organism, p: Pos) = {
