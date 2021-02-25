3604af252f61233d77a819d1a23c4eb0c994dc2d
Vanille-N <neven.villani@gmail.com>
2021-02-25 09:49:48 +0100

    BFS search with 4D memoization

--- src/main/scala/movement.scala
+++ src/main/scala/movement.scala
@@ -2,6 +2,7 @@ import Math._
 import scala.collection.mutable.Buffer
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.Set
+import scala.collection.mutable.Queue
 import scala.swing._
 import javax.swing.BorderFactory._
 import java.awt.Font
@@ -41,6 +42,41 @@ object Behavior extends Enumeration {
 import Behavior._
 
 class PathFinder (val envt: Array[Array[Boolean]], val rows: Int, val cols: Int) {
+    val distance = Array.ofDim[Array[Array[Int]]](rows, cols)
+    for (i <- 0 to rows - 1; j <- 0 to cols - 1) distance(i)(j) = null
+    def calcDistances (i: Int, j: Int) {
+        // BFS from (i,j)
+        val dists = Array.ofDim[Int](rows, cols)
+        for (k <- 0 to rows - 1; l <- 0 to cols - 1) dists(k)(l) = -1
+        val q = new Queue[Tuple3[Int, Int, Int]]
+        q.enqueue(Tuple3(i, j, 0))
+        dists(i)(j) = 0
+        println("Calculating distances from (" + i + "," + j + ")")
+        while (!q.isEmpty) {
+            val (k, l, dist) = q.dequeue
+            if (envt(k)(l)) {
+                println("Reached (" + k + "," + l + ")")
+                for (dk <- -1 to 1; dl <- -1 to 1) {
+                    if ((dk, dl) != (0, 0)) {
+                        val nk = k + dk
+                        val nl = l + dl
+                        println("Considering (" + nk + "," + nl + ")")
+                        if (0 <= nk && nk < rows && 0 <= nl && nl < cols && dists(nk)(nl) == -1) {
+                            dists(nk)(nl) = dist + 1
+                            q.enqueue(Tuple3(nk, nl, dist + 1))
+                        }
+                    }
+                }
+            }
+        }
+        for (i <- 0 to rows - 1) {
+            for (j <- 0 to cols - 1) {
+                print(dists(i)(j) + " ")
+            }
+            println()
+        }
+        distance(i)(j) = dists
+    }
     def getDistance (iCurrent: Int, jCurrent: Int, iTarget: Int, jTarget: Int): Int = {
         // println("Checking (" + iTarget + "," + jTarget + ")")
         if (distance(iTarget)(jTarget) == null) {
