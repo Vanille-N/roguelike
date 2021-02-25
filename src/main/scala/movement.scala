3861f5361f3924b87e1f096d0b0bf9713ab6d6dc
Vanille-N <neven.villani@gmail.com>
2021-02-25 10:04:53 +0100

    adjust distance calculations when passing through walls is required

--- src/main/scala/movement.scala
+++ src/main/scala/movement.scala
@@ -54,17 +54,19 @@ class PathFinder (val envt: Array[Array[Boolean]], val rows: Int, val cols: Int)
         println("Calculating distances from (" + i + "," + j + ")")
         while (!q.isEmpty) {
             val (k, l, dist) = q.dequeue
-            if (envt(k)(l)) {
-                println("Reached (" + k + "," + l + ")")
-                for (dk <- -1 to 1; dl <- -1 to 1) {
-                    if ((dk, dl) != (0, 0)) {
-                        val nk = k + dk
-                        val nl = l + dl
-                        println("Considering (" + nk + "," + nl + ")")
-                        if (0 <= nk && nk < rows && 0 <= nl && nl < cols && dists(nk)(nl) == -1) {
-                            dists(nk)(nl) = dist + 1
-                            q.enqueue(Tuple3(nk, nl, dist + 1))
-                        }
+            println("Reached (" + k + "," + l + ")")
+            for (dk <- -1 to 1; dl <- -1 to 1) {
+                if ((dk, dl) != (0, 0)) {
+                    val nk = k + dk
+                    val nl = l + dl
+                    println("Considering (" + nk + "," + nl + ")")
+                    if (
+                        0 <= nk && nk < rows && 0 <= nl && nl < cols // valid position
+                        && dists(nk)(nl) == -1 // not reached yet
+                        && (!envt(k)(l) || envt(nk)(nl))
+                    ) {
+                        dists(nk)(nl) = dist + 1
+                        q.enqueue(Tuple3(nk, nl, dist + 1))
                     }
                 }
             }
