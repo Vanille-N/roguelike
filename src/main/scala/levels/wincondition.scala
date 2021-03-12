3200a70c0f66aaf852a4a2d30db040f9595878c8
Vanille-N <neven.villani@gmail.com>
2021-03-12 18:36:00 +0100

    win by position implemented

--- src/main/scala/levels/wincondition.scala
+++ src/main/scala/levels/wincondition.scala
@@ -12,7 +12,21 @@ extends Reactor with Publisher {
 }
 
 class WinByPosition(body: BodyPart)
-extends WinCondition(body) {}
+extends WinCondition(body) {
+    val pos = body.room.locs(25, 25)
+    def explanation = "To complete this level, conquer the marked tile\n(stay for 50 turns on it with a strength >100)"
+    var count = 0
+    listenTo(body)
+    reactions += {
+        case LoopStep() => {
+            pos.notification
+            if (pos.strength(1) > 100) {
+                count += 1
+                if (count == 50) win
+            } else count = 0
+        }
+    }
+}
 
 class WinByPickup(body: BodyPart)
 extends WinCondition(body) {}
