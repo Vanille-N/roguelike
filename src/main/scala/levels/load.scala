b40bd7bee2945e6c262ec258cc30994f54e48ae9
Vanille-N <neven.villani@gmail.com>
2021-03-26 23:15:46 +0100

    impl loadFile

--- src/main/scala/levels/load.scala
+++ src/main/scala/levels/load.scala
@@ -4,6 +4,25 @@ import MakeItem._
 
 object GameLoader {
     def loadFile (f: String): CompactGame = {
+        println(s"<load $f>")
+        val src = Source.fromFile(s"assets/$f.save")
+        val lines = src.getLines
+        val level = lines.next.toInt
+        var inventory = new CompactInventory()
+        for (line <- lines) {
+            val entry = line.split(" ")
+            val id = entry(0) match {
+                case "ALCOHOL" => ALCOHOL
+                case "MOVE" => MOVE
+                case "JAVEL" => JAVEL
+                case "HEAT" => HEAT
+                case "SPIKE" => SPIKE
+                case "LEAK" => LEAK
+                case "MEMBRANE" => MEMBRANE
+            }
+            inventory.contents(id) = entry(1).toInt
+        }
+        new CompactGame(level, inventory)
     }
 
     def tryLoadFile (f: String): CompactGame = {
