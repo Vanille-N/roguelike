4b1b3b78db8b68d1d79a36365c0c6d45a74c7ffd
Vanille-N <neven.villani@gmail.com>
2021-05-18 16:06:19 +0200

    default values when absent file

--- client/src/main/scala/config.scala
+++ client/src/main/scala/config.scala
@@ -17,43 +17,20 @@ import event._
 
 object Config {
     def fromFile (file: String): Socket = {
+        var spec: Tuple3[String, Int, Int] = null
         try {
             val src = Source.fromFile("client.cfg")
             val line = src.getLines.next.split(" ")
-            val spec = startGame(line(0), line(1).toInt, line(2).toInt)
-            val host = spec._1
-            val port = spec._2
-            val timeout = spec._3
-            val socket = validateSocket(host, port, timeout)
-            println(s"Listening on $host:$port")
-            socket
+            spec = startGame(line(0), line(1).toInt, line(2).toInt)
         } catch {
-            case e: java.lang.NumberFormatException => {
-                println(s"$e")
-                println("When reading configuration file client.cfg")
-                println("Provide a valid integer")
-                println("Syntax:")
-                println("        host port timeout")
-                println("e.g.    localhost 8001 20000")
-                sys.exit(1)
-            }
-            case e: java.io.FileNotFoundException => {
-                println(s"$e")
-                println("Configuration must be provided in client.cfg")
-                println("Provide host, port, timeout")
-                println("e.g.")
-                println("$ cat config.cfg")
-                println("localhost 8001 20000")
-                sys.exit(1)
-            }
-            case e: java.lang.ArrayIndexOutOfBoundsException => {
-                println(s"$e")
-                println("Not enough configuration informations were provided")
-                println("Please indicate host, port, and timeout, separated by a single space")
-                println("e.g.    localhost 8001 20000")
-                sys.exit(1)
-            }
+            case e: Throwable => spec = startGame("127.0.0.1", 8000, 20000)
         }
+        val host = spec._1
+        val port = spec._2
+        val timeout = spec._3
+        val socket = validateSocket(host, port, timeout)
+        println(s"Listening on $host:$port")
+        socket
     }
 
 	def validateSocket(
